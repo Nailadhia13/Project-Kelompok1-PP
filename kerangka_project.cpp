4529780b01f34ea84e8765df5ee929dfd055add8
Nailadhia13 <nailadhia07@gmail.com>
2025-11-15 23:36:06 +0700

    ngerapihin aja

--- kerangka_project.cpp
+++ kerangka_project.cpp
@@ -1,43 +1,39 @@
-//============================== WELCOME TO OUR PROGRAM, KAK!!===================================
-//============================ BUDGETBUDDY : KODT DAILY TRACKER =================================
-//================================== KELOMPOK 1 : ILKOMP B ======================================
-
-#include <iostream> // untuk input output program
-#include <string> // untuk tipe data string
+#include <iostream> // untuk input output
+#include <string> // untuk pakai tipe data string
 #include <iomanip> // untuk format angka desimal
-#include <fstream> // untuk membuat/membaca/menulis file
+#include <fstream> // untuk baca dan nulis file
 using namespace std;
 
 // ================== DEKLARASI DAN INISIALISASIIN DULU VARIABEL GLOBALNYA ==================
-double saldo = 0; // kenapa diinisialisasiin dari 0? karena saldo user dimulai dari 0
-string pengeluaran[100]; // pake panjang array 100 ya untuk antisipasi ajasi gamungkin juga kan pengeluaran user sampe 100 wkwk banyak bet buat apa aje coba
-double jumlahPengeluaran[100]; // menyesuaikan banyaknya pengeluaran, misal pengeluarannya buat makan, nah ini input jumlah pengeluarannya
-int jumlahData = 0; // data pengeluaran yang diinputkan oleh user
+double saldo = 0; // pake double karena uang bisa aja punya nilai desimal
+string pengeluaran[100]; // untuk nyimpen nama pengeluarannya user (misal: buat makan, buat gojek, dll)
+double jumlahPengeluaran[100]; // untuk nyimpen harga atau jumlah dari pengeluarannya
+int jumlahData = 0; // untuk nyimpen ada berapa data pengeluaran yang udah tercatat, makanya dimukai dari 0
 
 // ================== FUNCTION BUAT BIKIN FILE BARU SECARA LANGSUNG ==================
-void simpanData() { // kenapa pake void? karena ini fungsi tanpa mengembalikan nilai, sekadar untuk menyimpan data ke file
-	ofstream file ("keuangan.txt"); // membuat file yg namanya "keuangan.txt"
+void simpanData() { // fungsi tanpa mengembalikan nilai, cuma untuk menyimpan data ke file
+	ofstream file ("keuangan.txt"); // buka atau buat file untuk nyimpen data pengeluaran, whiich is nama filenya "keuangan.txt"
 	
-	if (file.is_open()){ // ngecek apakah file udah berhasil kebuka apa belom
-		file << saldo << endl; // menyimpan saldo ke dalam file di baris pertama
+	if (file.is_open()){ //memastikan file berhasil dibuka
+		file << saldo << endl; // nyimpen saldo di baris pertama, lalu pindah baris
 		//nah abis itu, nantikan user nginput tuh pengeluarannya
 		//nanti otomatis dalam file ini ke list pengeluaran beserta nilai pengeluarannya
 		for ( int i = 0; i < jumlahData; i++) { // untuk nulis semua data pengeluaran satu persatu
 			file << pengeluaran[i] << " = " << jumlahPengeluaran[i] << endl; // format penulisan yang bakal tampil di file
 		}	
-		file.close(); //abis user nginput filenya bakal ketutup
-    	cout << "Data berhasil disimpan ke 'keuangan.txt'\n";
+		file.close(); // nutup file dan mastiin supaya data beneran kesimpen
+    	cout << "Data berhasil disimpan ke 'keuangan.txt'\n"; //kalau is_opennya true maka data akan disimpan
 	} else { // kalau is_opennya false maka masuk ke else dan tampilannya bakal begini :
 		cout << "Gagal menyimpan data!\n";
 	}
 }
 // ================== FUNCTION UNTUK MEMUAT DATA DARI FILE ==================
-void muatData() { //baca data dari file ke memory program
-	ifstream file("keuangan.txt"); //ifstream buat apa hayo? yaa betul, buat buka file keuangan.txt yg dh kita buat
+void muatData() { // fungsi untuk baca data dari file ke memori program
+	ifstream file("keuangan.txt"); // buka dulu dong filenya biar kebaca
 	
-	if(file.is_open()) { // kalau file berhasil dibuka
-		file >> saldo; // ini akan ngebaca data di file dari barisan pertama
-		file.ignore(); //lewati sisa newline biar pembacaan baris berikutnya dengan getline ga bermasalah
+	if(file.is_open()) { //kalau file berhasil dibuka
+		file >> saldo; // baca salo dari baris pertama
+		file.ignore(); // ngelewati newline biar pembacaaan baris berikutnya dengan getline ga bermasalah
 		
 		string line;
 		jumlahData = 0;
@@ -67,37 +63,37 @@ void tampilkanMenu() {
     cout << "4. Lihat Daftar Pengeluaran\n";
     cout << "5. Simpan dan Keluar\n";
     cout << "==================================================\n";
-    cout << "Pilih dulu dong menunya (1-5): ";
+    cout << "Hai anak maniez, silahkan mau pilih yang mana? (1-5): ";
 }
 
-// FUNCTUION TAMBAH PEMNASUKAN
+// ================= FUNCTUION TAMBAH PEMNASUKAN =================
 void tambahPemasukan() {
 double jumlah;
-cout << "Masukkan jumlah pemasukan : Rp ";
+cout << "Ciee mau nabung, mau nabung berapa nich : Rp ";
 cin >> jumlah;
 saldo += jumlah;
-cout << "? Pemasukan sebesar Rp" << fixed << setprecision(2) << jumlah << "Berhasil ditambahkan" << endl; 
+cout << "Pemasukan sebesar Rp" << fixed << setprecision(2) << jumlah << " Berhasil ditambahkan!!! OTW JADI SULTAN NIH" << endl; 
 }
 
 // ================== FUNCTION CATAT PENGELUARAN ==================
 void catatPengeluaran() {
     double jumlah;
 	string tujuan;
-	cout << "Masukkan jumlah uang pengeluaran: Rp ";
+	cout << "Masukkan nominal, EH TAPI KALO MAU JADI SULTAN GABOLE BOROS YA!! : Rp ";
 	cin >> jumlah;
 	cin.ignore();
-	cout << "Untuk apa uangnya dipakai (misal: makan, pulsa, laundry)? ";
+	cout << "Untuk apa uangnya dipakai (misal: makan, kuota, laundry) : ";
 	getline(cin, tujuan);
 
 	// ==== PERCABANGAN: cek saldonya dah cukup pa blom ====
 	if(jumlah > saldo){
-		cout << "? Saldo tidak cukup untuk pengeluaran ini!\n";
+		cout << "Saldo tidak cukup untuk pengeluaran ini! MAKANYA JGN BOROS!!! \n";
 	} else {
 		saldo -= jumlah;
 		pengeluaran[jumlahData] = tujuan;
 		jumlahPengeluaran[jumlahData] = jumlah;
 		jumlahData++;
-		cout << "? Pengeluaran Rp" << jumlah << " untuk  '" << tujuan << "' telah dicatat.\n";
+		cout << "Pengeluaran Rp" << jumlah << " untuk  '" << tujuan << "' telah dicatat.\n";
 	}
 }
 
@@ -105,7 +101,8 @@ void catatPengeluaran() {
 void ringkasan() {
     cout << "\n========== RINGKASAN KEUANGAN ==========\n";
     cout << "Saldo saat ini : Rp" << fixed << setprecision(2) << saldo << endl;
-
+	cout << "Bentar lagi jadi sultan wkwk" << endl; 
+	
     double total = 0;
     for (int i = 0; i < jumlahData; i++) {
         total += jumlahPengeluaran[i]; 
@@ -115,10 +112,12 @@ void ringkasan() {
     cout << "Jumlah transaksi  : " << jumlahData << " kali\n";
     cout << "========================================\n";
 }
+
+// ================== FUNCTION LIHAT DAFTAR PENGELUARAN ==================
 void lihatDaftar() {
     cout << "\n========== DAFTAR PENGELUARAN ==========\n";
 
-    // ==== PERULANGAN UNTUK MENAMPILKAN DATA ====
+// ================= PERULANGAN UNTUK MENAMPILKAN DATA ===================
     for (int i = 0; i < jumlahData; i++) {
         cout << i + 1 << ". " << pengeluaran[i]
              << " - Rp" << fixed << setprecision(2)
@@ -132,7 +131,7 @@ void lihatDaftar() {
     cout << "========================================\n";
 }
 
-// ================== FUNGSI UTAMA ==================
+// ================== FUNGSI UTAMA ===============================
 int main() {
     muatData(); 
     int pilihan;
